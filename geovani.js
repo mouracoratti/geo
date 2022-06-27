0e0d83bff2ba19c7d1b85633ea6fbf8ace63042f
mouracoratti <105809199+mouracoratti@users.noreply.github.com>
2022-06-27 14:19:55 -0300

    Update and rename geovani to geovani.js

--- geovani.js
+++ geovani.js
@@ -1,7 +1,8 @@
 function funçâo(){
-let nome = prompt("qual o seu nome ? ");
-let idade = Number(prompt("qual a sua idade ?"));
-let pessoas = Number(prompt("quantas pessoas estão envolvidas? "));
+let nome = ["Geovani"];
+let idade = ["19"];
+let pessoas = ["80"];
+let data = ["04"];
 //verificar as idades
 if(idade >= 18){
 console.log("sua idade é " + 18 + "pode passar");
@@ -13,7 +14,14 @@ if(pessoas >= 100){
 console.log("não pode passar");
 }else{
 //senão pode passar
-console.log("pode passar");
+console.log("pode passar " + nome);
 console.log("numero de participantes " + pessoas);
 }
+  if(idade >= 18, pessoas <= 100, data <= 05){
+    console.log("Deu certo");
+    pessoas++;
+    console.log("pessoas "+ pessoas);
+  }else{
+   console.log("deu errado"); 
+  }
 }
